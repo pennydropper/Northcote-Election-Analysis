a58d18d4810fd174d75589ebdec9e12f19dcbecf
James Northrop <pennydropper@gmail.com>
2019-10-26 16:14:00 +1100

    Ingest distribution preferences

--- code/functions.R
+++ code/functions.R
@@ -27,3 +27,31 @@ plot_disn_sh <- function(yr = "2017") {
   # pref_point
   
 }
+
+
+page_length <- function(url, css_sel) {
+  # Calculates the html_nodes length that would be returned for the URL and CSS selectors
+  read_html(url) %>% html_nodes(css_sel) %>% length()
+}
+
+dist_pref_url_ <- function(p_domain, p_yr, p_dist_pref_pg_nm, p_electorate_nm, css_sel) {
+  # Returns a vector with possible distribution preference URLs
+  dist_url_ <- vector("character", 2)
+  
+  dist_url_[1] <- str_c(p_domain, p_yr, "/",  p_dist_pref_pg_nm, p_electorate_nm, "District.html", sep = "")
+  # e.g. https://www.vec.vic.gov.au/Results/State2018/distributionNorthcoteDistrict.html
+  dist_url_[2] <- str_c(p_domain, p_yr,  p_dist_pref_pg_nm, p_electorate_nm, "District.html", sep = "")
+  # e.g. https://www.vec.vic.gov.au/Results/state2010distributionNorthcoteDistrict.html
+  
+  if (page_length(dist_url_[1], css_sel) > 0) {
+    # First page is legitmate
+    dist_url_[1]
+  } else if (page_length(dist_url_[2], css_sel) > 0) {
+    # Alt page is legitimate
+    dist_url_[2]
+  } else {
+    NA_character_
+  }
+  
+}
+
